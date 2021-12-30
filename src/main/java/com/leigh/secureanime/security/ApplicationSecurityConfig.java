817ec1e946e3a6565e13e107d6c4721552e81afc
Leigh Edwards <luton66@gmail.com>
2021-12-30 15:20:07 +0000

    Creating a new User with UserDetailsService and InMemoryUserDetailsManager

--- src/main/java/com/leigh/secureanime/security/ApplicationSecurityConfig.java
+++ src/main/java/com/leigh/secureanime/security/ApplicationSecurityConfig.java
@@ -1,13 +1,27 @@
 package com.leigh.secureanime.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -21,4 +35,20 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 //                This is what you use if you want to have form login instead
 //                .formLogin()
     }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        System.out.println ("TEST - " + passwordEncoder.encode("testpass"));
+
+        UserDetails user = User.builder()
+                .username("Leigh")
+                .password(passwordEncoder.encode("testpass"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    
 }
