b8f0f1401a911a2a7d7030feb236a7c114bbe3c4
Leigh Edwards <luton66@gmail.com>
2021-12-30 17:23:56 +0000

    restrict endpoint access to a certain role

--- src/main/java/com/leigh/secureanime/security/ApplicationSecurityConfig.java
+++ src/main/java/com/leigh/secureanime/security/ApplicationSecurityConfig.java
@@ -28,8 +28,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http    .authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/js/*")
-                .permitAll()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(USER.name())
                 .anyRequest()
                 .authenticated()
                 .and()
